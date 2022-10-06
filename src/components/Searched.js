832c397732397f2c18b18afbafd2413993686048
wanjiku <annritawanjiku77@gmail.com>
2022-10-06 22:34:12 +0300

    add styling

--- src/components/Searched.js
+++ src/components/Searched.js
@@ -25,8 +25,35 @@ function Searched() {
 
 
   return (
-    <div>Searched</div>
+    <Grid>
+      <h3>Here are results for your search</h3>
+      
+    </Grid>
   )
 }
+
+const Grid = styled.div `
+    display:grid;
+    grid-gap:3rem;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+
+`;
+
+const Card = styled.div`
+ img{
+    width: 100%;
+    border-radius:2rem;
+
+ }
+ a{
+    text-decoration: none;
+ }
+ h4{
+    text-align:center;
+    padding: 1rem;
+ }
+`;
+export default Cuisine
+
 
 export default Searched
