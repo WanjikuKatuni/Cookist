0298634ca7b8ce8079969a9a1ac9d9fc3fda3f04
wanjiku <annritawanjiku77@gmail.com>
2022-10-06 17:02:20 +0300

    add wrapper and splide slide

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,7 +1,11 @@
 // component for popular foods
 
 import React, { useState, useEffect } from 'react'
-
+// component that has styling
+import styled from 'styled-components';
+// add carosel and splide styling
+import { Splide, SplideSide, SplideSlide } from '@splidejs/react-splide';
+import "@splidejs/splide/dist/css/splide.min.css";
 
 
 function Popular() {
@@ -29,22 +33,54 @@ function Popular() {
 
 
   return (
-    <div>
+    <Wrapper>
         <h3>Popular recipes</h3>
+        {/* spide for carousel - show 4 images per page*/}
+        <Splide options={{
+            perPage:4,
+            // remove arrows
+            arrows: false,
+            // remove page
+            pagination: false,
+            drag:"free",
+            gap:"5rem",
+
+            }}>
         {/* looping over the list and output recipes */}
         {popularData.map((recipe)=>{
             return(
-                <div key={recipe.id}> 
+                <SplideSlide>
+                <Card key={recipe.id}> 
                     
                     {/* lists all titles of the recipe */}
                     <p>{recipe.title}</p>
                     <img src={recipe.image} alt={recipe.title}/>
-                </div>
+                </Card>
+                </SplideSlide>
             )
         })};
+        </Splide>
 
-    </div>
+    </Wrapper>
   )
 }
+
+
+// styles
+const Wrapper = styled.div `
+margin: 4rem 0rem;
+`;
+
+const Card = styled.div `
+broder-radius: 2rem;
+min-height:2rem;
+overflow: hidden;
+
+img {
+    border-radius: 2rem;
+}
+`;
+
+
 
 export default Popular
