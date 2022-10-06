402533cccb715a6ae1f14d6b57985ae525d5d10f
wanjiku <annritawanjiku77@gmail.com>
2022-10-06 23:16:52 +0300

    add styling

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -24,10 +24,59 @@ function Recipe() {
 
 
   return (
-    <div>
-        {recipeDetails.title}
-    </div>
+    <DetailWrapper>
+        {/* {recipeDetails.title} */}
+        <div>
+            <h2>{recipeDetails.title}</h2>
+            <img src={recipeDetails.image} alt={recipeDetails.title}/>
+        </div>
+        <Info>
+            <Button>
+                Instructions
+            </Button>
+            <Button>
+                Ingredients
+            </Button>
+        </Info>
+    </DetailWrapper>
   )
 }
+
+
+const DetailWrapper = styled.div `
+    margin-top:10rem;
+    margin-bottom: 5rem;
+    display:flex;
+    .active{
+        background: linear-gradient(35deg, #494949, #313131);
+        color: white;
+    }
+
+    h2{
+        margin-bottom: 2rem;
+    }
+    li {
+        font-size: 1.2rem;
+        line-height: 2.5rem;
+
+    }
+    ul{
+        margin-top: 2rem;
+    }
+`;
+
+const Button = styled.button`
+    padding: 1rem 2rem;
+    color: #313131;
+    background: white;
+    font-weight:600;
+    border: 2px solid black;
+    margin-right: 2rem;
+
+`;
+
+const Info= styled.div `
+    margin-left:10rem;
+`;
 
 export default Recipe
