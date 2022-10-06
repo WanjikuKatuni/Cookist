3500b409ef3cbe385fcf14410ec8a5fa9d8c51e1
wanjiku <annritawanjiku77@gmail.com>
2022-10-06 23:00:38 +0300

    added get recipeDetails fn to fetch recipes

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components'
 
 function Recipe() {
+
+    const params = useParams();
+    const [recipeDetails, setRecipeDetails]=useState({});
+
+    useEffect(()=>{
+        getRecipeDetails();
+    },[params.name])
+
+    function getRecipeDetails(){
+        fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_RECIPE_API_KEY}`)
+        .then((response)=>response.json())
+        .then((detailsData)=>{
+            setRecipeDetails(detailsData)
+            console.log(detailsData)
+    });
+    }
+
+
+
+
   return (
     <div>Recipe</div>
   )
