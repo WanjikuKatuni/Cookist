fa735481e3171dc778119f5f89d1943e3de2d075
wanjiku <annritawanjiku77@gmail.com>
2022-10-06 20:17:52 +0300

    styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,10 +8,36 @@ import {FaSearch} from 'react-icons/fa';
 function Search() {
   return (
     // Insert form
-    <form>
+    <StyleForm>
         <input type="text"/>
-    </form>
+    </StyleForm>
   )
 }
+
+
+const StyleForm = styled.form `
+     margin: 0rem 20rem;
+     position: relative;
+     width:100%;
+     input{
+        border:none;
+        background: linear-gradient(35deg, #494949,#313131);
+        font-size: 1.5rem;
+        color:white;
+        padding: 1rem 3rem;
+        border:none;
+        border-radius: 1rem;
+        outline:none;
+
+        svg {
+            postion:absolute;
+            top:50%;
+            left:0%;
+            transform: translate(100%,-50%);
+            color:white;
+        }
+
+     }
+`;
 
 export default Search
