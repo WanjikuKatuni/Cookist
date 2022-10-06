244ea0c7118a40e68f83b6fe0df1c5c1473c2492
wanjiku <annritawanjiku77@gmail.com>
2022-10-06 13:29:21 +0300

    added function to get popular recipes

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,10 +1,47 @@
 // component for popular foods
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+
 
 function Popular() {
+
+    // get popular data
+    const [popularData, setPopularData] = useState([]);
+    
+    
+    useEffect(()=>{
+        getPopularRecipes();
+    }, []);
+
+
+    function getPopularRecipes(){
+
+        fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_RECIPE_API_KEY}&number=10`)
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log(data.recipes)
+            setPopularData(data.recipes)
+        });
+    }
+
+   
+
+
   return (
-    <div>Popular</div>
+    <div>
+        Popular
+
+        {/* looping over the list and output recipes */}
+        {popularData.map((recipe)=>{
+            return(
+                <div> 
+                    <p>{recipe.title}</p>
+                </div>
+            )
+        })}
+
+    </div>
   )
 }
 
