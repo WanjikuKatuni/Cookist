d30cecbd1fe7efe9fbf5a2897f85f9aa1ab68750
wanjiku <annritawanjiku77@gmail.com>
2022-10-07 16:41:11 +0300

    icon styling

--- src/App.js
+++ src/App.js
@@ -28,21 +28,24 @@ function App() {
 
 
 const Nav = styled.div`
-  padding: 4rem 0;
+  padding: 0rem 4rem;
   display: flex;
   justify-content: flex-start;
   align-items: center;
+  font-family: 'Inspiration', cursive; 
+  
+
   
   svg {
-    font-size: 2rem;
+    font-size: 6rem;
   }
 `;
 
 const Logo = styled(Link)`
   text-decoration: none;
-  font-size: 1.5rem;
+  font-size: 8rem;
   font-weight: 400;
-  // font-family: "Lobster Two", cursive;
+  color: black;
 `;
 
 export default App;
